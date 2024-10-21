68cd6c3369242252998c8254ed26f06f726e75e5
Alessio Glorioso <56506324+Aglorios17@users.noreply.github.com>
2024-10-21 10:56:43 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+echo " ######### Docker install #########"
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker --version
+sudo docker run hello-world
+sudo usermod -aG docker $USER
+echo " ######### Docker install finished #########"
