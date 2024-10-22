71ac4c2bcbfa13aa773054293e45a53eb55ca23d
Aglorios17 <alessio.glorioso17@gmail.com>
2024-10-22 10:52:12 +0200

    Update Docker setup script to include additional Docker packages and improve installation process

--- Docker-setup.sh
+++ Docker-setup.sh
@@ -1,13 +1,25 @@
-echo " ######### Docker install #########"
-sudo apt install ca-certificates curl gnupg lsb-release
-sudo mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo tee /etc/apt/keyrings/docker.gpg > /dev/null
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo docker --version
+echo "# Add Docker's official GPG key:"
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo "# Add the repository to Apt sources:"
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+echo "# Install Docker:"
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+echo "# Verify that Docker is installed correctly by running the hello-world image:"
 sudo docker run hello-world
+echo "# Add your user to the docker group:"
+sudo groupadd docker
 sudo usermod -aG docker $USER
-sudo systemctl enable docker
-echo " ######### Docker install finished #########"
-#sudo reboot
+newgrp docker
+docker run hello-world
+echo "# Enable and start the Docker service:"
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
